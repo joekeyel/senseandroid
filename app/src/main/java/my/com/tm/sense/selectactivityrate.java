6159b4dc7e5f972942a4ebce18f5ed8141f9c25d
Hasanul Isyraf <hasanul2307@gmail.com>
2018-10-25 15:14:35 +0800

    add 10 activity

---
+++ app/src/main/java/my/com/tm/sense/selectactivityrate.java
@@ -0,0 +1,18 @@
+package my.com.tm.sense;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class selectactivityrate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selectactivityrate);
+
+
+
+
+
+    }
+}
